68fa672c822d4faa9c080b63532badb8468a9085
stooged <stooged@users.noreply.github.com>
2021-02-21 09:01:47 +0800

    Initial commit

---
+++ PS5_Server/PS5_Server.ino
@@ -0,0 +1,87 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WebServerSecure.h>
+#include <DNSServer.h>
+#include <FS.h>
+
+DNSServer dnsServer;
+ESP8266WebServer webServer(80);
+ESP8266WebServerSecure sWebServer(443);
+
+static const char serverCert[] = "-----BEGIN CERTIFICATE-----\r\nMIIC1DCCAj2gAwIBAgIUFQgjEtkNYfmrrpNQKHVNl3+dl08wDQYJKoZIhvcNAQEL\r\nBQAwfDELMAkGA1UEBhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExEDAOBgNVBAcM\r\nB0ZyZW1vbnQxDDAKBgNVBAoMA2VzcDEMMAoGA1UECwwDZXNwMQwwCgYDVQQDDANl\r\nc3AxHDAaBgkqhkiG9w0BCQEWDWVzcEBlc3AubG9jYWwwHhcNMjEwMjIxMDAwMDQ4\r\nWhcNNDMwNzI4MDAwMDQ4WjB8MQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZv\r\ncm5pYTEQMA4GA1UEBwwHRnJlbW9udDEMMAoGA1UECgwDZXNwMQwwCgYDVQQLDANl\r\nc3AxDDAKBgNVBAMMA2VzcDEcMBoGCSqGSIb3DQEJARYNZXNwQGVzcC5sb2NhbDCB\r\nnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAsrfFqlV5H0ajdAkkZ51HTOseOjYj\r\nNiaUD4MA5mIRonnph6EKIWb9Yl85vVa6yfVkGn3TFebQ96MMdTfZgLuP4ryCwe6Y\r\n+tZs2g6TjGbR0O6yuA8wQ2Ln7E0T05C8oOl88SGNV4tVL6hz64oMzuVebVDo0J9I\r\nybvL0O/LhMvC4x8CAwEAAaNTMFEwHQYDVR0OBBYEFCMQIU+pZQDVySXejfbIYbLQ\r\ncLXiMB8GA1UdIwQYMBaAFCMQIU+pZQDVySXejfbIYbLQcLXiMA8GA1UdEwEB/wQF\r\nMAMBAf8wDQYJKoZIhvcNAQELBQADgYEAFHPz3YhhXQYiERTGzt8r0LhNWdggr7t0\r\nWEVuAoEukjzv+3DVB2O+56NtDa++566gTXBGGar0pWfCwfWCEu5K6MBkBdm6Ub/A\r\nXDy+sRQTqH/jTFFh5lgxeq246kHWHGRad8664V5PoIh+OSa0G3CEB+BXy7WF82Qq\r\nqx0X6E/mDUU=\r\n-----END CERTIFICATE-----";
+static const char serverKey[] = "-----BEGIN PRIVATE KEY-----\r\nMIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALK3xapVeR9Go3QJ\r\nJGedR0zrHjo2IzYmlA+DAOZiEaJ56YehCiFm/WJfOb1Wusn1ZBp90xXm0PejDHU3\r\n2YC7j+K8gsHumPrWbNoOk4xm0dDusrgPMENi5+xNE9OQvKDpfPEhjVeLVS+oc+uK\r\nDM7lXm1Q6NCfSMm7y9Dvy4TLwuMfAgMBAAECgYEApKFbSeyQtfnpSlO9oGEmtDmG\r\nT9NdHl3tWFiydId0fTpWoKT9YwWvdnYIB12klbQicbDkyTEl4Gjnafd3ufmNsaH8\r\nZ9twopIdvvWDvGPIqGNjvTYcuczpXmQWiUnG5OTiVWI1XuZa3uZEGSFK9Ra6bE4g\r\nG2xklGZGdaqqcd6AVhECQQDnBXVXwBxExxSFppL8KUtWgyXAvJAEvkzvTOQfcCel\r\naIM5EEUofB7WZeMtDEKgBtoBl+i5PP+GnDF0zsjDFx2nAkEAxgqVQii6zURSVE2T\r\niJDihySXJ2bmLJUjRIi1nCs64I9Oz4fECVvGwZ1XU8Uzhh3ylyBSG2HjhzA5sTSC\r\n1a/tyQJAOgE12EWFE4PE1FXhm+ymXN9q8DyoEHjTilYNBRO88JwQLpi2NJcNixlj\r\n8+CbLeDqhfHlXfVB10OKa2CsKce5CwJAbhaN+DQJ+3dCSOjC3YSk2Dkn6VhTFW9m\r\nJn/UbNa/KPug9M5k1Er3RsO/OqsBxEk7hHUMD3qv74OIXpBxNnZQuQJASlwk5HZT\r\n7rULkr72fK/YYxkS0czBDIpTKqwklxU+xLSGWkSHvSvl7sK4TmQ1w8KVpjKlTCW9\r\nxKbbW0zVmGN6wQ==\r\n-----END PRIVATE KEY-----";
+
+String AP_SSID = "PS5_WEB_AP";
+String AP_PASS = "password";
+String PS5_REGION = "gb"; // jp, us, au, uk, eu, kr, sa, tw, ru, mx, cn
+
+IPAddress Server_IP(10,1,1,1);
+IPAddress Subnet_Mask(255,255,255,0);
+
+
+
+void sendindex()
+{
+    String tmphtm = "<!DOCTYPE html><html><head><style>body { background-color: #1451AE;color: #ffffff;font-size: 14px; font-weight: bold; margin: 0 0 0 0.0; padding: 0.4em 0.4em 0.4em 0.6em;}</style></head><center><br><br><br><br><br><br>ESP</center></html>";
+    webServer.setContentLength(tmphtm.length());
+    webServer.send(200, "text/html", tmphtm);
+}
+
+
+
+void redirectToHTTP() {
+  sWebServer.sendHeader("Location", String("http://" + Server_IP.toString() + "/index.html" ), true);
+  sWebServer.send(301, "text/plain", "");
+}
+
+
+
+void setup(void) 
+{
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  Serial.println("SSID: " + AP_SSID);
+  Serial.println("Password: " + AP_PASS);
+  Serial.println("WEB Server IP: " + Server_IP.toString());
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(Server_IP, Server_IP, Subnet_Mask);
+  WiFi.softAP(AP_SSID.c_str(), AP_PASS.c_str());
+  Serial.println("WIFI AP started");
+
+
+  dnsServer.setTTL(30);
+  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+  dnsServer.start(53, "*", Server_IP);
+  Serial.println("DNS server started");
+
+  
+  webServer.on("/index.html", HTTP_GET, sendindex);
+  webServer.onNotFound([]() {
+  webServer.send(404, "text/plain", "Not Found");
+  Serial.println("[HTTP] " + webServer.uri());
+  });
+
+
+
+  sWebServer.getServer().setRSACert(new X509List(serverCert), new PrivateKey(serverKey));
+  sWebServer.on("/document/" + PS5_REGION + "/ps5/", HTTP_GET, redirectToHTTP);
+  sWebServer.onNotFound([]() {
+  sWebServer.send(404, "text/plain", "Not Found");
+  Serial.println("[HTTPS] " + sWebServer.uri());
+  });
+
+
+
+  sWebServer.begin();
+  webServer.begin();
+  Serial.println("HTTP server started");
+}
+
+
+void loop(void) {
+  dnsServer.processNextRequest();
+  webServer.handleClient();
+  sWebServer.handleClient();
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PS5-Server
  
+This is a project designed for the <a href=https://wiki.wemos.cc/products:retired:d1_mini_pro_v1.0.0>esp8266 D1 Mini PRO V1</a> to provide a wifi http server and dns server.<br>
+the esp8266 server will redirect the user guide https request to the servers internal http index page.
+<br><br>
+
+
+Settings for the D1 Mini PRO<br>
+The CPU frequency must be <b>160 MHz</b>
+<img src=https://github.com/stooged/PS5-Server/blob/master/images/board-info.jpg><br><br>
